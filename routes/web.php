ad4d44705190d3bdac436c2344bd960e6807d827
HU <heqiangboy@126.com>
2019-04-27 06:41:39 +0000

    分类下的话题列表

--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,6 @@ Route::get('email/resend', 'Auth\VerificationController@resend')->name('verifica
 // 个人中心页面
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+
+//分类下的话题列表
+Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
