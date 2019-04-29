a6d8e6b8964a081eeb8dec484ce2b50e47681f2c
HU <heqiangboy@126.com>
2019-04-29 02:04:31 +0000

    站点权限部署

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,6 +35,12 @@
     @include('layouts._footer')
 </div>
 
+
+    @if (app()->isLocal())
+        @include('sudosu::user-selector')
+    @endif
+
+
 <!-- Scripts -->
 <script src="{{ mix('js/app.js') }}"></script>
 
